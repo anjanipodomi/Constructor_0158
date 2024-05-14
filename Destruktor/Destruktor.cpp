c5a56786b0908145738b896e0b536d4af89285f1
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-14 13:21:05 +0700

    membuat angka::angka dan angka::~angka

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -12,9 +12,19 @@ public:
 	void isiData();
 };
 
+//Definisi member function
+angka::angka(int i) {	//Constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
 
-
-
+angka::~angka() {	//Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
 
 
 
