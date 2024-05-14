a9267326162ac8a5b96672b405f297273e85b219
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-14 12:59:15 +0700

    membuat objek class Mahasiswa

---
+++ function/function.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID(); // Metode untuk menetapkan ID
+	void printAll(); // Metode untuk mencetak detail
+
+	static void setNim(int pNim) { nim = pNim; } // Fungsi anggota statis untuk mengatur nim
+	static int getNim() { return nim;  }  // Fungsi anggota statis untuk mendapatkan nilai nim
+
+	// Konstruktor yang menerima nama dan menginisialisasi objek
+	Mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    
+}
